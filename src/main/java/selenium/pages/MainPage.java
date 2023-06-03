14c72ca4ccf9907f51be1991bc9c651da6dc7436
ilya_krotov <ilyakrotov58@gmail.com>
2023-06-03 17:33:24 +0400

    fear: were added test for Login Page

--- src/main/java/selenium/pages/MainPage.java
+++ src/main/java/selenium/pages/MainPage.java
@@ -3,11 +3,9 @@ package selenium.pages;
 import lombok.Data;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.Iterator;
 import java.util.Set;
 
 @Data
